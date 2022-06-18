2dd231e6cd7cf7339393e2fe4299437cf96cbbec
Benjamin Tinturier <btinturi@c2r7s8.42lausanne.ch>
2022-06-18 19:16:59 +0200

    change on ft.h creation of grid struc

--- ex00/ft.h
+++ ex00/ft.h
@@ -6,12 +6,57 @@
 
 int	ft_atoi(char *str);
 
-#endif
 
-#if 1
-# ifndef DEBUG
-#  define DEBUG
-#  include <stdio.h>
+//structure de lobjet grille
+struct s_grid
+{
+	char	**grid_solv;
+	int		**col;
+	int		**row;
+	int		*int_left;
+	size_t	size;
+}s_var_grid;
 
-# endif
+//init grille
+//pour int_left Left[0]  represete 1, donc[n] represente n-1
+int	init_grid(struct *grid, int n, size_t len)
+{
+	int	nb;
+
+	nb = 0;
+	grid->size = len;
+	grid->grid_solv[n][n];
+	grid->col[n][2];
+	grid->row[2][n];
+	grid->int_left[n];
+	if (!grid || (n < 4 && n > 9))
+	{
+		write(1, "Error", 5);
+		write(1, "\n", 1);
+		return (1);
+	}
+	while (nb < n)
+	{
+		int_left[nb] = n;
+		nb++;	
+	}
+	return (0);
+}
+
+//vidage objet grille
+int	empty_grid(struct *arr[], size_t len)
+{
+	int	i;
+
+	i = 0;
+	if (!grid)
+	{
+		return (1);
+	}
+	while (i < len)
+	{
+		free(arr[i]);
+	}
+	return (0);
+}
 #endif
